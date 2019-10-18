fe5499c344a5bf93d6aea6a0a78ae7b2db24fa5b
Jordan Morales <jordan.morales@gmail.com>
2019-10-18 08:34:04 -0700

    prepared first loop for reviewing large array of information, ie moves; wireframes added to img folder

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-// console.log($);
-// tested working
-
 $(() => {
 
   $('input[type="submit"]').click(() => {
@@ -14,18 +11,29 @@ $(() => {
         url: "https://pokeapi.co/api/v2/pokemon/"+$userInput
       }).then(
         (data) => {
-          // console.log(data.sprites.front_default);
-          //using to determing placement as docs aren't on point
-
-
+          console.log(data.moves);
           //pulls data of name, weight, and first type (need a loop for multiple types as they share an array)
+          // loop example
+          // let $moves = data.moves;
+          //   for (var i = 0; i < $moves.length; i++) {
+          //     console.log($moves[i].move.name);
+          //   }
         $('#sprite').attr('src', data.sprites.front_default)
         $('#name').html(data.name);
         $('#weight').html(data.weight);
         $('#type').html(data.types[0].type.name);
+        $('#move0').html(data.moves[0].move.name);
+          // example using loop
+          // $('#move1').html($moves[1].move.name);
         () => {
             console.log('bad request');
         }
       })
   }); //closing input click
 }) //closing jquery
+
+
+
+
+// graveyard
+//using to determing placement as docs aren't on point
