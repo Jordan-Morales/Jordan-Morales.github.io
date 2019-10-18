03d04bb3b82a048bb7696a048a07e02c11331cbe
Jordan Morales <jordan.morales@gmail.com>
2019-10-17 21:37:21 -0700

    basic pokemon search function, lookup by name or number

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 // console.log($);
 // tested working
+
+$(() => {
+
+  $('input[type="submit"]').click(() => {
+    let $userInput = $('input[type="text"]').val();
+
+    event.preventDefault(); // required
+    // currently ajax is now dependant on input to determine query
+      $.ajax({
+        //this ajax url is done by pokedex number or name (ie 43 or oddish)
+        url: "https://pokeapi.co/api/v2/pokemon/"+$userInput
+      }).then(
+        (data) => {
+          // console.log(data.sprites.front_default);
+          //using to determing placement as docs aren't on point
+
+
+          //pulls data of name, weight, and first type (need a loop for multiple types as they share an array)
+        $('#sprite').attr('src', data.sprites.front_default)
+        $('#name').html(data.name);
+        $('#weight').html(data.weight);
+        $('#type').html(data.types[1].type.name);
+        () => {
+            console.log('bad request');
+        }
+      })
+  }); //closing input click
+}) //closing jquery
